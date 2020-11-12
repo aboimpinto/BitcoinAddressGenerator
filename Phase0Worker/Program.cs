7263f0b3d408e6adc057f7af9c608328c9b0aeaa
Paulo Aboim Pinto <aboimpinto@gmail.com>
2020-11-12 19:43:30 +0100

    Phase0 stream consumer worker project created

---
+++ Phase0Worker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phase0Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
